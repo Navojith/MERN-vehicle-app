ce9b12c1cf1e3f2d75bd314f0798759d795c1cbc
Navojith <navojith22@gmail.com>
2023-08-16 16:32:51 +0530

    feat: show biding

--- src/components/BidList.jsx
+++ src/components/BidList.jsx
@@ -3,11 +3,32 @@ import { useEffect, useState } from 'react';
 
 const BidList = () => {
   const { bid, dispatch } = useBidContext();
+  const [bidItems, setBidItems] = useState([]);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    console.log(bid);
+    setBidItems(bid);
   }, []);
-  return <div>{bid}</div>;
+  return (
+    <>
+      {bidItems &&
+        bidItems.map((item) => (
+          <div key={item.vehicle.id} className="text-white m-4">
+            <img src={item.vehicle.details.image} />
+            <h2>
+              {item.vehicle.details.brand +
+                ' ' +
+                item.vehicle.name +
+                ' - ' +
+                item.vehicle.details.manufactureYear}
+            </h2>
+
+            <p></p>
+          </div>
+        ))}
+      <h1>Total : {total} lkr</h1>
+    </>
+  );
 };
 
 export default BidList;
